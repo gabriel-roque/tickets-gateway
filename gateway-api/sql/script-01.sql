5b77b6ae1c1eaa16271ea768c1ae7691a3136d58
gabriel-roque <gabrielrms.dll@gmail.com>
2025-03-27 17:58:34 -0300

    ⚙️ chore: update infra sql

--- gateway-api/sql/script-01.sql
+++ gateway-api/sql/script-01.sql
@@ -1,14 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-create table if not exists transaction_pix
+create table transaction_pix
 (
-    id          uuid default uuid_generate_v4() not null
+    id          uuid    default uuid_generate_v4() not null
         constraint transaction_pix_pk
             primary key,
-    name        varchar(100)                    not null,
-    external_id varchar(100)                    not null,
-    value       integer                         not null,
-    qr_code     varchar(255)
+    name        varchar(100)                       not null,
+    external_id varchar(100)                       not null,
+    value       integer                            not null,
+    qr_code     varchar(255)                       not null,
+    status      boolean default false              not null
 );
 
 alter table transaction_pix
