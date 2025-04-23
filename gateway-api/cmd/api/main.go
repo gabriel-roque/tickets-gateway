6c3bb1feba9366b28c70687cbf91d8079cb3b07c
gabriel-roque <gabrielrms.dll@gmail.com>
2025-04-23 15:18:30 -0300

    ⚙️ chore: add elastic apm

--- gateway-api/cmd/api/main.go
+++ gateway-api/cmd/api/main.go
@@ -4,11 +4,13 @@ import (
 	"github.com/gabriel-roque/gateway-api/pkg/database"
 	"github.com/gabriel-roque/gateway-api/pkg/env"
 	"github.com/gin-gonic/gin"
+	"go.elastic.co/apm/module/apmgin/v2"
 )
 
 func main() {
 	env.LoadEnvs()
 	router := gin.Default()
+	router.Use(apmgin.Middleware(router))
 
 	router.GET("/healthy", func(c *gin.Context) {
 		ping := database.DatabasePing()
